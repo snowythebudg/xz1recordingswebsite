ab47a4704d600c70c0e5eb38f1c8f88417f89331
budgiemint <70620754+snowythebudg@users.noreply.github.com>
2025-01-01 18:01:00 -0700

    Changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -63,10 +63,9 @@ export default (() => {
 		</Section>
 		<Components.Footer>
 			<Override slot="text">
-				© 2024 XZ1 Recording Ventures
+				© 2025 XZ1 Recording Ventures
 				<br />
 				Owned and operated by BudgieMint (https://ebb.l5.ca&#125;
-				<br />
 			</Override>
 		</Components.Footer>
 		<Link
