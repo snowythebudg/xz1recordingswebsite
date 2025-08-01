9aedea6b9b42d1e2fec06e8fbb0c640419c7030b
budgiemint <snowythebudg@gmail.com>
2025-07-31 20:30:42 -0600

    Changes

--- src/pages/welcomenewclient.js
+++ src/pages/welcomenewclient.js
@@ -89,7 +89,12 @@ export default (() => {
 				<br />
 				Notice about demos:
 				<br />
-				Please send a demo to the proper channels (our demo portal or Discord). We will not accept demos sent to one of our emails!{" "}
+				Please send a demo to the proper channels (our demo portal or Discord). We will not accept demos sent to one of our emails!
+			</Override>
+			<Override slot="text9">
+				© 2025 XZ1 Recording Ventures
+				<br />
+				Site version 1.25.6 (build 07313){"\n\n\n\n"}
 			</Override>
 		</Components.Footer0731>
 		<Link
